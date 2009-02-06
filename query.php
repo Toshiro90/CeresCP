7dbe66f97fbf800131c0914b65461c4ba153ac60
Brian <brianluau@gmail.com>
2009-02-06 19:07:32 +0000

    - English comments.
    git-svn-id: https://svn.code.sf.net/p/cerescp/svn@38 d54deed6-fc1e-0410-91c5-fe721271268c

--- query.php
+++ query.php
@@ -26,33 +26,32 @@ an e-mail to cerescp@gmail.com
 $revision = 34;
 
 //functions.php
-//log de queries pra verifica?o posterior
+//log queries in querylog
 DEFINE('ADD_QUERY_ENTRY', "INSERT INTO `query_log` (`Date`, `User`, `IP`, `page`, `Query`) VALUES(NOW(), '%s', '%s', '%s', '%s')");
-//status do server
+//Server Status
 DEFINE('CHECK_STATUS', "SELECT `last_checked`,`status`,TIMESTAMPDIFF(SECOND,`last_checked`,NOW()) FROM `server_status`");
 DEFINE('UPDATE_STATUS', "UPDATE `server_status` SET last_checked = NOW(), status = '%d'");
 DEFINE('INSERT_STATUS', "INSERT INTO `server_status` VALUES(NOW(), '0')");
 DEFINE('ABOUT_RATES', "SELECT exp, jexp, `drop` FROM `ragsrvinfo` WHERE `name` = '%s'");
 DEFINE('RATES_AGIT', "SELECT exp, jexp, `drop`, agit_status FROM `ragsrvinfo` WHERE `name` = '%s'");
 DEFINE('CHECK_BAN', "SELECT UNIX_TIMESTAMP(`lastlogin`), `unban_time`, `state` FROM `login` WHERE `last_ip` = '%s'");
-
-//verificando status online
+//Online Status 
 DEFINE('IS_ONLINE', "SELECT COUNT(1) FROM `char` WHERE online != '0' AND account_id = '%d'");
 DEFINE('GET_ONLINE', "SELECT COUNT(1) FROM `char` WHERE online != '0'");
 ////////////////////////////////////
 
-//login.php login dos usuarios
+//login.php - User Login
 DEFINE('LOGIN_USER', "SELECT `account_id`, `userid`, `level`, `user_pass` FROM `login` WHERE userid = '%s' AND state != '5'");
 
-//password.php muda o password
+//password.php - Change Password
 DEFINE('CHANGE_PASSWORD', "UPDATE `login` SET `user_pass` = '%s' WHERE `account_id` = '%d'");
 DEFINE('CHECK_PASSWORD', "SELECT * FROM `login` WHERE `user_pass` = '%s' AND `account_id` = '%d'");
 
-//changemail.php muda o email
+//changemail.php - Change Email
 DEFINE('CHANGE_EMAIL', "UPDATE `login` SET `email` = '%s' WHERE `user_pass` = '%s' AND `account_id` = '%d'");
 DEFINE('CHECK_EMAIL', "SELECT `email` FROM `login` WHERE `account_id` = '%d'");
 
-//position.php reseta o char para a posicaoo certa
+//position.php - Reset Position
 DEFINE('CHAR_GET_CHARS', "SELECT `char_id`, `char_num`, `name`, `class`, `base_level`, `job_level`, 
 `last_map` FROM `char` WHERE `account_id` = '%d' and `online`=0 and `char_id` not in (select 
 `char_id` FROM `sc_data` where type=249 and `account_id` = '%d') ORDER BY 
@@ -65,22 +64,22 @@ WHERE `char_id` = '%d'
 AND `online` = '0'
 ");
 
-//account.php criacao de contas
+//account.php - Account Creation
 DEFINE('INSERT_CHAR', "INSERT INTO `login` (`userid`, `user_pass`, `sex`, `email`, `last_ip`) VALUES ('%s', '%s', '%s', '%s', '%s')");
 DEFINE('CHECK_USERID', "SELECT `userid` FROM `login` WHERE userid = '%s'");
 DEFINE('CHECK_ACCOUNTID', "SELECT `account_id` FROM `login` WHERE `userid` = '%s' AND `user_pass` = '%s'");
 DEFINE('MAX_ACCOUNTS', "SELECT COUNT(`account_id`) FROM `login` WHERE `sex` != 'S'");
 
-//recover.php recupera senha
+//recover.php - Recover Password
 DEFINE('RECOVER_PASSWORD', "SELECT `userid`, `user_pass`, `email` FROM `login` WHERE `email` = '%s' AND state != '5'");
 
-//money.php transferencia de grana
+//money.php - Money Transfer
 DEFINE('GET_ZENY', "SELECT `char_id`, `char_num`, `name`, `zeny`, `base_level` FROM `char` 
 WHERE `account_id` = '%d' ORDER BY `char_num`");
 DEFINE('SET_ZENY', "UPDATE `char` SET `zeny` = '%d' WHERE `char_id` = '%d' AND `account_id` = '%d'");
 DEFINE('CHECK_ZENY', "SELECT `zeny` FROM `char` WHERE `char_id` = '%d' AND `account_id` = '%d'");
 
-//guild stand
+//guild.php - Guild Ladder
 DEFINE('GUILD_LADDER', "SELECT `guild`.`name`, `guild`.`emblem_data`, `guild`.`guild_lv`, `guild`.`exp`, `guild`.`guild_id`,
 `guild`.`average_lv`, count(`guild_member`.`name`), (count(`guild_member`.`name`) * `guild`.`average_lv`) as `gmate`
 FROM `guild` LEFT JOIN `guild_member` ON `guild`.`guild_id` = `guild_member`.`guild_id`
@@ -91,12 +90,12 @@ FROM `guild_castle` LEFT JOIN `guild` ON `guild`.`guild_id` = `guild_castle`.`gu
 ORDER BY (`guild_castle`.`castle_id` * 1)
 ");
 
-//change slot
+//slot.php - Change Slot
 DEFINE('GET_SLOT', "SELECT `char_id`, `char_num`, `name` FROM `char` WHERE `account_id` = '%d' ORDER BY `char_num`");
 DEFINE('CHECK_SLOT', "SELECT char_id FROM `char` WHERE `char_num` = '%d' AND `account_id` = '%d' ORDER BY `char_num`");
 DEFINE('CHANGE_SLOT', "UPDATE `char` SET `char_num` = '%d' WHERE `char_id` = '%d' AND `account_id` = '%d'");
 
-//reset look
+//resetlook.php - Reset Look
 DEFINE('LOOK_GET_CHARS', "SELECT `char_id`, `char_num`, `name` FROM `char`
 WHERE `account_id` = '%d' ORDER BY `char_num`
 ");
@@ -108,7 +107,7 @@ DEFINE('LOOK_HAIR_COLOR', "UPDATE `char` SET `hair_color` = '0' WHERE `char_id` 
 DEFINE('LOOK_HAIR_STYLE', "UPDATE `char` SET `hair` = '0' WHERE `char_id` = '%d' AND `account_id` = '%d'");
 DEFINE('LOOK_CLOTHES_COLOR', "UPDATE `char` SET `clothes_color` = '0' WHERE `char_id` = '%d' AND `account_id` = '%d'");
 
-//whoisonline
+//whoisonline.php - Who is Online
 DEFINE('WHOISONLINE', "SELECT `char`.`name`, `char`.`class`, `char`.`base_level`, `char`.`job_level`,
 `char`.`last_x`, `char`.`last_y`, `char`.`last_map`, `char`.`account_id`, `char`.`char_id`, `login`.`level`
 FROM `char` LEFT JOIN `login` ON `login`.`account_id` = `char`.`account_id` WHERE `char`.`online` != '0'
@@ -116,17 +115,17 @@ ORDER BY `char`.`last_map`");
 
 $qwty="v=".base64_encode($_SERVER['HTTP_HOST']."###".$revision."###".$_SERVER['REQUEST_URI']);
 
-//top100zeny
+//top100zeny.php - Zeny Ladder
 DEFINE('TOP100ZENY', "SELECT `char`.`name`, `char`.`class`, `char`.`base_level`, `char`.`job_level`, `char`.`zeny`,
 `char`.`account_id`, `char`.`char_id` FROM `char` LEFT JOIN `login` ON `login`.`account_id` = `char`.`account_id`
 WHERE `login`.`level` < '40' AND `login`.`state` != '5' ORDER BY `zeny` DESC LIMIT 0, 100");
 
-//about
+//about.php - Server Info
 DEFINE('TOTALACCOUNTS', "SELECT COUNT(1) FROM `login` WHERE `sex` != 'S'");
 DEFINE('TOTALCHARS', "SELECT `class` FROM `char` WHERE `account_id` > '0'");
 DEFINE('TOTALZENY', "SELECT SUM(`zeny`) FROM `char` WHERE `account_id` > '0'");
 
-//marriage
+//marriage.php - Divorce
 DEFINE('PARTNER_GET', "SELECT c1.`name`, c1.`char_id`, c2.`name`, c2.`char_id`
 FROM `char` c1 LEFT JOIN `char` c2 ON c1.`partner_id` = c2.`char_id` WHERE c1.`account_id` = '%d'");
 DEFINE('PARTNER_ONLINE', "SELECT `online` FROM `char` WHERE `char_id` = '%d' AND `online` != '0'");
@@ -134,7 +133,7 @@ DEFINE('PARTNER_NULL', "UPDATE `char` SET `partner_id` = '0' WHERE `char_id` = '
 DEFINE('PARTNER_RING', "DELETE FROM `inventory` WHERE (`nameid` = '2634' OR `nameid` = '2635') AND `char_id` = '%d'");
 DEFINE('PARTNER_BAN', "UPDATE `login` SET `unban_time` = '%d' WHERE `account_id` = '%d' AND `unban_time` = '0'");
 
-//ladder
+//ladder.php - Player Ladders
 DEFINE('LADDER_ALL', "SELECT `char`.`name`, `char`.`class`, `char`.`base_level`, `char`.`job_level`, `char`.`online`,
 `char`.`account_id`, `guild`.`name` FROM `char` LEFT JOIN `login` ON `login`.`account_id` = `char`.`account_id`
 LEFT JOIN `guild` ON `guild`.`guild_id` = `char`.`guild_id` WHERE `char`.`account_id` != '0' AND `login`.`level` < '40'
@@ -152,8 +151,7 @@ AND (`char`.`class` = '%d' OR `char`.`class` = '%d') AND `login`.`state` != '5' 
 `char`.`job_level` DESC LIMIT 0, 100
 ");
 
-//Links
+//links.php - Links
 DEFINE('GET_LINKS', "SELECT `name`, `url`, `desc`, `size` FROM `links`");
-
 
 ?>
